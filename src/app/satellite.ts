e65e4651cbc34e46017f516ef5c8e778ce9c1d43
Amy Do <amy@Amys-MBP.attlocal.net>
2021-06-30 17:32:30 -0500

    modified satellite ts

---
+++ src/app/satellite.ts
@@ -0,0 +1,11 @@
+export class Satellite {
+
+constructor (name: string, type: string, launchDate: string, orbitType: string, operational: boolean)
+
+// name: string;
+// orbitType: string;
+// type: string;
+// operational: boolean;
+// launchDate: string;
+
+}
