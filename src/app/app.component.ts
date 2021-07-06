3faf3e799575ec367558ed70f7967792c109a7c9
Amy Do <amy@Amys-MBP.attlocal.net>
2021-07-06 13:49:38 -0500

    cleaned up code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,17 +14,6 @@ export class AppComponent {
   sourceList: Satellite[];
   displayList: Satellite[];
 
-      // constructor() {
-      //   this.sourceList = [
-      //     new Satellite("SiriusXM", "Communication", "2009-03-21", "LOW", true),
-      //     new Satellite("Cat Scanner", "Imaging", "2012-01-05", "LOW", true),
-      //     new Satellite("Weber Grill", "Space Debris", "1996-03-25", "HIGH", false),
-      //     new Satellite("GPS 938", "Positioning", "2001-11-01", "HIGH", true),
-      //     new Satellite("ISS", "Space Station", "1998-11-20", "LOW", true),
-      //   ];
-      //   this.displayList = this.sourceList.slice(0);
-      // }
-
 //Part 5: Fetch Satellite Data
       constructor() {
         this.sourceList = [];
